450b3f2dd0bcbf050808bd715fada85f7ce1fecf
sumamovva <movvasuma101@gmail.com>
2019-05-31 19:45:12 +0530

    Add and set up acts_as_votable

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'devise', '~> 4.2'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'jquery-rails'
+gem 'acts_as_votable', '~> 0.10.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
- devise_for :installs
- resources :links
-
- root to: "links#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 	devise_for :users
+  resources :links do
+    member do
+      put "like",    to: "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+  end
+ 
+  
+  root "links#index"
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
